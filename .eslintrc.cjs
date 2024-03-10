f67b0739670fae780c2a9c95aeb01dce1d9785aa
TkTioNG <tktiong@icloud.com>
2024-03-10 21:08:59 +0800

    feat: first commit

---
+++ .eslintrc.cjs
@@ -0,0 +1,71 @@
+module.exports = {
+  extends: [
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react/recommended",
+    "plugin:react-hooks/recommended",
+  ],
+  plugins: ["import", "react", "jsx-a11y"],
+  rules: {
+    "import/no-anonymous-default-export": "warn",
+    "react/no-unknown-property": "off",
+    "react/react-in-jsx-scope": "off",
+    "react/prop-types": "off",
+    "jsx-a11y/alt-text": [
+      "warn",
+      {
+        elements: ["img"],
+        img: ["Image"],
+      },
+    ],
+    "jsx-a11y/aria-props": "warn",
+    "jsx-a11y/aria-proptypes": "warn",
+    "jsx-a11y/aria-unsupported-elements": "warn",
+    "jsx-a11y/role-has-required-aria-props": "warn",
+    "jsx-a11y/role-supports-aria-props": "warn",
+    "react/jsx-no-target-blank": "off",
+  },
+  parserOptions: {
+    requireConfigFile: false,
+    sourceType: "module",
+    allowImportExportEverywhere: true,
+  },
+  overrides: [
+    {
+      files: ["**/*.ts?(x)"],
+      parser: "@typescript-eslint/parser",
+      parserOptions: {
+        sourceType: "module",
+        ecmaFeatures: {
+          jsx: true,
+        },
+        warnOnUnsupportedTypeScriptVersion: true,
+      },
+    },
+  ],
+  settings: {
+    react: {
+      version: "detect",
+    },
+    "import/parsers": {
+      [require.resolve("@typescript-eslint/parser")]: [
+        ".ts",
+        ".mts",
+        ".cts",
+        ".tsx",
+        ".d.ts",
+      ],
+    },
+    "import/resolver": {
+      [require.resolve("eslint-import-resolver-node")]: {
+        extensions: [".js", ".jsx", ".ts", ".tsx"],
+      },
+      [require.resolve("eslint-import-resolver-typescript")]: {
+        alwaysTryTypes: true,
+      },
+    },
+  },
+  env: {
+    browser: true,
+    node: true,
+  },
+};
